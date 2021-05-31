708ebe41f6cc44908b479f58045d18790f069681
AmirHossein Yari <ahyari2002@gmail.com>
2021-05-31 15:01:25 +0430

    grass updated

--- Grass.java
+++ Grass.java
@@ -10,9 +10,18 @@ public class Grass {
     public int getY() {
         return Y;
     }
+    public void setX(int x) {
+        X = x;
+    }
+    public void setY(int y) {
+        Y = y;
+    }
 
     public Grass(int x, int y) {
         X = x;
         Y = y;
+    }
+
+    public Grass() {
     }
 }
