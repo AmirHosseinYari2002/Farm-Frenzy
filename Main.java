c6f97fe194d16f92113d791ade27422e284fa957
AmirHossein Yari <ahyari2002@gmail.com>
2021-07-18 15:58:26 +0430

    Revert "Revert "files updated for graphic works.""
    This reverts commit 23f389af3b281b97bb82890f269c5619d39b8ce5.

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        GameHandler gameHandler = new GameHandler();
-        gameHandler.startGame();
+//        GameHandler gameHandler = new GameHandler();
+//        gameHandler.startGame();
+        GameGUI gameGUI = new GameGUI();
+        gameGUI.loadApp();
+        gameGUI.StartMenu();
     }
 }
